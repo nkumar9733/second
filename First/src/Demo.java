bf713e856aa14ac3aa39f2d320ffa76f7b0a004f
BEAST <BEAST@BEAST>
2019-12-14 15:27:43 +0530

    Updated Demo.

--- First/src/Demo.java
+++ First/src/Demo.java
@@ -4,5 +4,6 @@ public class Demo
 	public static void main(String[] args) {
 		System.out.println("Hello Welcome to java World....");
 		System.out.println("Do it now");
+		System.out.println("This is how it works.");
 	}
 }
